d02decf07010d14f827ee0aef272fd13a6f347b0
lortega <lortega@birt.eus>
2022-11-09 23:08:25 +0100

    Change class test Point to GeoJsonPoint

--- src/test/java/birt/eus/getyourroutebackend/model/GeoLocationTest.java
+++ src/test/java/birt/eus/getyourroutebackend/model/GeoLocationTest.java
@@ -1,25 +1,22 @@
 package birt.eus.getyourroutebackend.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.mongodb.client.model.geojson.Point;
-import com.mongodb.client.model.geojson.Position;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 class GeoLocationTest {
 
   @Test
   void testSerialization() throws JsonProcessingException {
     GeoLocation geoLocation =
-      new GeoLocation(LocalDateTime.now(), "id", "id", new Point(new Position(List.of(43D, -2D))));
+      new GeoLocation(LocalDateTime.now(), "id", "id", new GeoJsonPoint(43D, -2D));
+     // new GeoLocation(LocalDateTime.now(), "id", "id", new Point(new Position(List.of(43D, -2D))));
     ObjectMapper mapper = new ObjectMapper()
       .registerModule(new JavaTimeModule())
       .enable(SerializationFeature.INDENT_OUTPUT);
